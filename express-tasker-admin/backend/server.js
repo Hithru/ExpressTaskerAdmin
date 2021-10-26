35dc7e90df2bc8853d9f0889a7c994af03d4d80e
Hithru <56365929+Hithru@users.noreply.github.com>
2021-10-26 13:16:03 +0530

    Database Connection Stabilished

---
+++ express-tasker-admin/backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("config");
+
+require("dotenv").config();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
