6f2195c1aee0bbc9f73848ebd22a09de6eacba69
Hithru <56365929+Hithru@users.noreply.github.com>
2021-10-29 00:44:41 +0530

    Login Frontend Created Using Tailwind Template

--- express-tasker-admin/src/App.js
+++ express-tasker-admin/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import routes from "./routes";
-
+import auth from "./services/auth";
 import "./App.css";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+
+    this.setState({ user });
+  }
   render() {
     return <div className="App">{routes}</div>;
   }
--- express-tasker-admin/src/component/Login/login.js
+++ express-tasker-admin/src/component/Login/login.js
@@ -1,15 +1,75 @@
 import React, { Component } from "react";
+import { Link, NavLink } from "react-router-dom";
 import { Redirect } from "react-router-dom";
-
+import FooterSmall from "../Footer/Footer";
 import "./login.css";
 class Login extends Component {
   render() {
     return (
-      <div className="signup-window">
-        <div className="signup-form">
-          <h2>Customer Login</h2>
-        </div>
-      </div>
+      <main>
+        <section className="relative w-full h-full py-40 min-h-screen">
+          <div
+            className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
+            style={{
+              backgroundImage: "url(" + require("./tools.png").default + ")",
+            }}
+          ></div>
+          <div className="container mx-auto px-4 h-full">
+            <div className="flex content-center items-center justify-center h-full">
+              <div className="w-full lg:w-4/12 px-4">
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+                  <div className="rounded-t mb-0 px-6 py-6">
+                    <div className="btn-wrapper text-center"></div>
+                    <hr className="mt-6 border-b-1 border-blueGray-300" />
+                  </div>
+
+                  <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                    <form>
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlFor="grid-password"
+                        >
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          placeholder="Email"
+                        />
+                      </div>
+
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlFor="grid-password"
+                        >
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          placeholder="Password"
+                        />
+                      </div>
+
+                      <div className="text-center mt-6">
+                        <button
+                          className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                          type="button"
+                        >
+                          Sign In
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <FooterSmall absolute />
+        </section>
+      </main>
     );
   }
 }
