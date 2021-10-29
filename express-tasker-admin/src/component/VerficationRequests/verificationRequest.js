f259520e0706ba497c3111c58611bb5c21175461
Hithru <56365929+Hithru@users.noreply.github.com>
2021-10-30 02:24:28 +0530

    Verification Request Basic Structure

---
+++ express-tasker-admin/src/component/VerficationRequests/verificationRequest.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+class VerificationRequest extends Component {
+  render() {
+    return (
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full mb-12 px-4"></div>
+        <div
+          className={
+            "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white "
+          }
+        ></div>
+
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <h3 className={"font-semibold text-lg text-blueGray-700"}>
+                Verification Requests
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="block w-full overflow-x-auto">
+          {/* Projects table */}
+          <table className="items-center w-full bg-transparent border-collapse">
+            <thead>
+              <tr>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                  }
+                >
+                  Service Provider
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                  }
+                >
+                  Description
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                  }
+                >
+                  Status
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                  }
+                >
+                  Resources
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                  }
+                >
+                  Actions
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                  }
+                ></th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VerificationRequest;
