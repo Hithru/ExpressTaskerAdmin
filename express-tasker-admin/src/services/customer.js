07c6b670d9151ff4a8b74aa4e9988fbd1edf4f6f
Hithru <56365929+Hithru@users.noreply.github.com>
2021-11-02 12:28:26 +0530

    Customer complaints table frontend and Connection to backend

---
+++ express-tasker-admin/src/services/customer.js
@@ -0,0 +1,21 @@
+import http from "./httpService";
+import { apiUrl } from "../config.json";
+
+const apiComplaintspoint = apiUrl + "/customer/complaints";
+
+export function getComplaints() {
+  return http.post(apiComplaintspoint);
+}
+
+const apiResolveEndpoint = apiUrl + "/customer/resolve";
+
+export function resolveComplaint(complaint_id) {
+  console.log(complaint_id);
+  const resolve_body = { complaint_id: complaint_id };
+  return http.post(apiResolveEndpoint, resolve_body);
+}
+
+export default {
+  getComplaints,
+  resolveComplaint,
+};
