b496045d7f8dd6b1e4b4e245e250356f8246351c
Hithru <56365929+Hithru@users.noreply.github.com>
2021-11-02 13:12:23 +0530

    Service Provider Complaints Frontend connected

--- express-tasker-admin/src/component/ServiceProviderComplaints/serviceProviderComplaints.js
+++ express-tasker-admin/src/component/ServiceProviderComplaints/serviceProviderComplaints.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
+import ServiceProvider from "../../services/serviceProvider";
 
 class ServiceProviderComplaints extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      complaints: [],
+    };
+  }
+
+  async componentDidMount() {
+    const complaintsArray = await ServiceProvider.getComplaints();
+    const complaints = complaintsArray.data;
+    this.setState({ complaints });
+  }
+
+  async handleResolved(complaint_id) {
+    const complaint = await ServiceProvider.resolveComplaint(complaint_id);
+    window.location = "/admin/serviceprovidercomplaints";
+  }
   render() {
+    const { complaints } = this.state;
     return (
       <div className="flex flex-wrap mt-4">
         <div className="rounded-t bg-white mb-0 px-6 py-6">
@@ -14,7 +34,7 @@ class ServiceProviderComplaints extends Component {
 
         <div className="block w-full overflow-x-auto">
           {/* Projects table */}
-          <table className="items-center w-full bg-transparent border-collapse">
+          <table className="items-center w-full bg-transparent border-collapse table-fixed ">
             <thead>
               <tr>
                 <th
@@ -29,28 +49,28 @@ class ServiceProviderComplaints extends Component {
                     "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
                   }
                 >
-                  Description
+                  EMAIL
                 </th>
                 <th
                   className={
                     "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
                   }
                 >
-                  Status
+                  DESCRIPTION
                 </th>
                 <th
                   className={
                     "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
                   }
                 >
-                  Attachments
+                  STATUS
                 </th>
                 <th
                   className={
                     "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
                   }
                 >
-                  Actions
+                  ACTIONS
                 </th>
                 <th
                   className={
@@ -59,6 +79,47 @@ class ServiceProviderComplaints extends Component {
                 ></th>
               </tr>
             </thead>
+            <tbody>
+              {complaints.map((item) => (
+                <tr>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                    {item.serviceProvider_name}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                    {item.serviceProvider_email}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                    <pre>{item.description}</pre>
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                    {item.isSolved && (
+                      <div>
+                        <i className="fas fa-circle text-emerald-500 mr-2"></i>{" "}
+                        completed
+                      </div>
+                    )}
+                    {!item.isSolved && (
+                      <div>
+                        <i className="fas fa-circle text-orange-500 mr-2"></i>{" "}
+                        pending
+                      </div>
+                    )}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                    {!item.isSolved && (
+                      <div>
+                        <button
+                          className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm  px-3 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                          onClick={() => this.handleResolved(item._id)}
+                        >
+                          Resolved
+                        </button>
+                      </div>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
