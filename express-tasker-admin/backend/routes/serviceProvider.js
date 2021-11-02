a31427dada5a1175b677dbfd89bb16a298256d96
Hithru <56365929+Hithru@users.noreply.github.com>
2021-11-02 20:58:01 +0530

    Verification Routes Created

--- express-tasker-admin/backend/routes/serviceProvider.js
+++ express-tasker-admin/backend/routes/serviceProvider.js
@@ -3,12 +3,21 @@ const {
   ServiceProviderComplaint,
 } = require("../models/serviceProviderComplaint.model");
 
+const ServiceProvider = require("../models/serviceprovider.model");
+const SkillVerification = require("../models/skillVerification.model");
+
 router.post("/complaints", async (req, res) => {
   const serviceProviderComplaints = await ServiceProviderComplaint.find().sort(
     "isSolved"
   );
   console.log(serviceProviderComplaints);
   res.send(serviceProviderComplaints);
+});
+
+router.post("/requests", async (req, res) => {
+  const verificationRequests = await SkillVerification.find().sort("isSolved");
+  console.log(verificationRequests);
+  res.send(verificationRequests);
 });
 
 router.post("/resolve", async (req, res) => {
@@ -26,5 +35,47 @@ router.post("/resolve", async (req, res) => {
       .send("The complaint with the given ID was not found.");
 
   res.send(complaint);
+});
+
+router.post("/decline", async (req, res) => {
+  console.log(req.body.request_id);
+  const request = await SkillVerification.findByIdAndUpdate(
+    req.body.request_id,
+    {
+      isSolved: true,
+    }
+  );
+
+  if (!request)
+    return res.status(404).send("The Request with the given ID was not found.");
+
+  res.send(request);
+});
+
+router.post("/accept", async (req, res) => {
+  console.log(req.body.request_id);
+  const request = await SkillVerification.findByIdAndUpdate(
+    req.body.request_id,
+    {
+      isSolved: true,
+      isaccepted: true,
+    }
+  );
+
+  const serviceProvider = await ServiceProvider.findByIdAndUpdate(
+    request.serviceProviderId,
+    {
+      isVerified: true,
+    }
+  );
+  if (!request)
+    return res.status(404).send("The Request with the given ID was not found.");
+
+  if (!serviceProvider)
+    return res
+      .status(404)
+      .send("The serviceProvider with the given ID was not found.");
+
+  res.send(request);
 });
 module.exports = router;
