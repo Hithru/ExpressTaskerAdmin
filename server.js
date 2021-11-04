4b15022fa06945b851b57a397d75eb6e1347d21d
Hithru <56365929+Hithru@users.noreply.github.com>
2021-11-04 15:05:10 +0530

    path variable added

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV !== "test") {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("express-tasker-admin/build"));
   // Express serve up index.html file if it doesn't recognize route
-  // const path = require('path');
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "express-tasker-admin", "build", "index.html")
