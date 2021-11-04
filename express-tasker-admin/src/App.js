d4752df4d0d1be3eeb7cb12250da9795fd9e7dae
Hithru <56365929+Hithru@users.noreply.github.com>
2021-11-04 14:49:35 +0530

    layout delete and add it to app.js

--- express-tasker-admin/src/App.js
+++ express-tasker-admin/src/App.js
@@ -7,7 +7,15 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import Login from "./component/Login/login";
 
-import Admin from "./layouts/Admin";
+import Sidebar from "./component/Sidebar/sidebar";
+import Dashboard from "./component/Dashboard/Dashboard";
+
+import LogOut from "./component/LogOut/logout";
+import VerificationRequest from "./component/VerficationRequests/verificationRequest";
+import CustomerComplaints from "./component/CustomerComplaint/customerComplaint";
+import AddAdmin from "./component/AddAdmin/addAdmin";
+import ServiceProviderComplaints from "./component/ServiceProviderComplaints/serviceProviderComplaints";
+import Admins from "./component/Admins/admins";
 
 class App extends Component {
   state = {};
@@ -28,9 +36,34 @@ class App extends Component {
           </Switch>
         )}
         {user && (
-          <Switch>
-            <Route path="/" component={Admin} />
-          </Switch>
+          <>
+            <Sidebar />
+
+            <div className="relative md:ml-64 bg-blueGray-100">
+              <Switch>
+                <Route path="/logout" component={LogOut} />
+                <Route path="/admin/dashboard" exact component={Dashboard} />
+                <Route
+                  path="/admin/verificationrequests"
+                  exact
+                  component={VerificationRequest}
+                />
+                <Route path="/admin/addadmin" exact component={AddAdmin} />
+                <Route
+                  path="/admin/customercomplaints"
+                  exact
+                  component={CustomerComplaints}
+                />
+                <Route
+                  path="/admin/serviceprovidercomplaints"
+                  exact
+                  component={ServiceProviderComplaints}
+                />
+                <Route path="/admin/admins" exact component={Admins} />
+                <Redirect from="/" to="/admin/dashboard" />
+              </Switch>
+            </div>
+          </>
         )}
       </div>
     );
